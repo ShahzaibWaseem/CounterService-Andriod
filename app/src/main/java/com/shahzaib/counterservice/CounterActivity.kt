2030b9a76852c95a2f65caacdb0e01a01b2e716d
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2020-09-09 10:03:08 +0500

    onStop implemented and receiver registered and unregistered in the activity

--- app/src/main/java/com/shahzaib/counterservice/CounterActivity.kt
+++ app/src/main/java/com/shahzaib/counterservice/CounterActivity.kt
@@ -3,10 +3,12 @@ package com.shahzaib.counterservice
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
 
 class CounterActivity: AppCompatActivity() {
     private lateinit var counterReceiver: CounterReceiver
@@ -19,9 +21,18 @@ class CounterActivity: AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+        counterReceiver = CounterReceiver()
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(CounterService.INTENT_ACTION)
+        registerReceiver(counterReceiver, intentFilter)
 
         val intent: Intent = Intent(this, CounterService::class.java)
         startService(intent)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(counterReceiver)
     }
 
     class CounterReceiver: BroadcastReceiver() {
